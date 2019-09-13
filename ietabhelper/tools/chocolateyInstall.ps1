db0081ecbcd77681b0c2a136db64034405a45ff1
Sebastien Boulianne <sebastien.boulianne@cpu.ca>
2019-09-13 17:49:53 -0400

    IE Tab Helper

---
+++ ietabhelper/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$url        = 'https://www.ietab.net/enterprise/ietabhelper.msi'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'ie tab helper'
+  checksum = '60D17F2BAF38B1F9654E6FF869A5B6CDD732F46F6FCA28DDEE263EC8FAD833C0'
+  checksumType  = 'sha256'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  
+}
+
+Install-ChocolateyPackage @packageArgs
