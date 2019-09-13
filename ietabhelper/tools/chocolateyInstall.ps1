f3a688eee5836b9700e4cf9db2a9f5673c2ca06d
Sebastien Boulianne <sebastien.boulianne@cpu.ca>
2019-09-13 18:23:43 -0400

    f

--- ietabhelper/tools/chocolateyInstall.ps1
+++ ietabhelper/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $url        = 'https://www.ietab.net/enterprise/ietabhelper.msi'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  fileType      = 'EXE'
+  fileType      = 'MSI'
   url           = $url
   softwareName  = 'ie tab helper'
   checksum = '60D17F2BAF38B1F9654E6FF869A5B6CDD732F46F6FCA28DDEE263EC8FAD833C0'
