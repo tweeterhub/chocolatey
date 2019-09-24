69c29bc22655910f32466b6216404d786993be4f
Sebastien Boulianne <Sebastien.Boulianne@CPU>
2019-09-24 10:22:48 -0400

    Aruba VIA Client

---
+++ arubaviaclient/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$url        = 'https://www.ietab.net/enterprise/ietabhelper.msi'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+  url           = $url
+  softwareName  = 'Aruba Networks Virtual Intranet Access Client*'
+  checksum = '60D17F2BAF38B1F9654E6FF869A5B6CDD732F46F6FCA28DDEE263EC8FAD833C0'
+  checksumType  = 'sha256'
+  silentArgs    = "/quiet"
+  validExitCodes= @(0)
+  
+}
+
+Install-ChocolateyPackage @packageArgs
