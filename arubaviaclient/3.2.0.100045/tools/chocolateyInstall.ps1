4032a13ffa06ef2da9c806c3157bfe582b98869a
Sebastien Boulianne <sebastien.boulianne@cpu.ca>
2019-09-24 11:32:44 -0400

    .

---
+++ arubaviaclient/3.2.0.100045/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'Aruba Networks Virtual Intranet Access Client'
+$Version = "3.2.0.100045"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = "http://h30326.www3.hpe.com/hpn/Aruba-VIA-3.2.0.100045_Win_32.msi?merchantId=ASP_DROPBOX"
+  url64bit      = "http://h30326.www3.hpe.com/hpn/Aruba-VIA-3.2.0.100045_Win_364.msi?merchantId=ASP_DROPBOX"
+
+  softwareName  = "Aruba Networks Virtual Intranet Access Client*"
+
+  checksum      = '261068B945D44CBE029D2553CACAF4665BC30EF31B321D82A25CD87A71E18F7C'
+  checksumType  = 'sha256'
+  checksum64    = '9E020EF32EA402EB1B546A0CE5C6A5460F608376F8650F7B74C6E1CD3B4FEF25'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+  }
+
+Install-ChocolateyPackage @packageArgs
