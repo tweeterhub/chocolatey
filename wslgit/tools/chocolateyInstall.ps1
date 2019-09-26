f559c6ddc24f4f3c0d3e243ec6882696c651610c
Sebastien Boulianne <sebastien.boulianne@cpu.ca>
2019-09-26 11:49:54 -0400

    WSLGit First

---
+++ wslgit/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  destination = Join-Path (Get-ToolsLocation) $env:ChocolateyPackageName
+  file        = "$toolsPath\cmder.7z"
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.7z -ea 0
+
+Install-ChocolateyPath $packageArgs['destination'] 'User'
