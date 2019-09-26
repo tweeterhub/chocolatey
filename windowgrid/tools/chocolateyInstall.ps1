9d3769eaa0a06ef8824d0a1f93620911b3416bcc
Sebastien Boulianne <sebastien.boulianne@cpu.ca>
2019-09-26 12:21:06 -0400

    .

--- windowgrid/tools/chocolateyInstall.ps1
+++ windowgrid/tools/chocolateyInstall.ps1
@@ -1,19 +1,14 @@
-﻿$ErrorActionPreference = 'Stop';
-$packageName= 'Aruba Networks Virtual Intranet Access Client'
-$Version = "3.2.0.100045"
+﻿$packageName = 'windowgrid'
+$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$tempDir = "$toolsDir\temp"
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'MSI'
-  url           = "http://h30326.www3.hpe.com/hpn/Aruba-VIA-3.2.0.100045_Win_32.msi?merchantId=ASP_DROPBOX"
-  url64bit      = "http://h30326.www3.hpe.com/hpn/Aruba-VIA-3.2.0.100045_Win_64.msi?merchantId=ASP_DROPBOX"
-  softwareName  = "Aruba Networks Virtual Intranet Access Client*"
-  checksum      = '261068B945D44CBE029D2553CACAF4665BC30EF31B321D82A25CD87A71E18F7C'
-  checksumType  = 'sha256'
-  checksum64    = '9E020EF32EA402EB1B546A0CE5C6A5460F608376F8650F7B74C6E1CD3B4FEF25'
-  checksumType64= 'sha256'
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
-  }
+    PackageName    = $packageName
+    Url64bit       = 'http://windowgrid.net/download/1.3.1.1'
+    Checksum64     = 'EF7558B2869A1F74B81E19F1F84C715D95ABCA819920E6882D467323506EA25F'
+    ChecksumType64 = 'sha256'
+    UnzipLocation  = $toolsDir
+}
 
-Install-ChocolateyPackage @packageArgs
+# Download and unzip into a temp folder
+Install-ChocolateyZipPackage @packageArgs
