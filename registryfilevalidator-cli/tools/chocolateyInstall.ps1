800755f0b3716c78423b8dea6edc18c97066abe8
Sebastien Boulianne <sebastien.boulianne@cpu.ca>
2019-10-01 11:39:12 -0400

    f

---
+++ registryfilevalidator-cli/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'registryfilevalidator-cli'
+$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    PackageName    = $packageName
+    Url64bit       = 'https://forum.xda-developers.com/attachment.php?attachmentid=397962&d=1284040833'
+    Checksum64     = 'FC6A2C54B92494B4230943260151107322E6233D9F4759B9884EB7C578AB143D'
+    ChecksumType64 = 'sha256'
+    UnzipLocation  = $toolsDir
+}
+
+# Download and unzip into a temp folder
+Install-ChocolateyZipPackage @packageArgs
