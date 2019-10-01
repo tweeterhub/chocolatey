467000514018b1a633822726572cbcd7b78a3687
Sebastien Boulianne <sebastien.boulianne@cpu.ca>
2019-10-01 13:12:06 -0400

    .

---
+++ wumgr/tools/chocolateyinstall.ps1
@@ -0,0 +1,38 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/DavidXanatos/wumgr/releases/download/0.9a/WuMgr_v0.9a.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'wumgr*'
+
+  checksum      = 'AC604E2A3ED6DB4C4285BD25DB561D19DF4474D81965C12E13454268CC248BB0'
+  checksumType  = 'sha256'
+
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
