6f17a67133a90046e4bd1f98ee50a83321922980
Sebastien Boulianne <sebastien.boulianne@cpu.ca>
2019-10-10 15:51:13 -0400

    .

---
+++ packages/iis-cors-module/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$url        = 'https://www.microsoft.com/web/handlers/webpi.ashx?command=getinstallerredirect&appid=cors'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'IIS CORS Module'
+  checksum = 'C5E398C323FD231DDD906BE375E5D52A25CE8A93BA6B79F1F6633B27849D9951'
+  checksumType  = 'sha256'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  
+}
+
+Install-ChocolateyPackage @packageArgs
