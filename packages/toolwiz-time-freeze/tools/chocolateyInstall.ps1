93190a024369841eeb921eb3dca13c0227e09a80
Sebastien Boulianne <sebastien.boulianne@cpu.ca>
2019-10-16 13:58:23 -0400

    .

---
+++ packages/toolwiz-time-freeze/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'ToolWiz Time Freeze'
+$Version = "4.3.1.5000"
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url64bit      = "http://www.toolwiz.com/downloadfiles/Setup_Timefreeze.exe"
+  softwareName  = "ToolWiz Time Freeze*"
+  checksum64    = 'D2636E005AA7683BEA4D5375901BAEE06F19B82EED129ED834E94D816C6F20D8'
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+  }
+
+Install-ChocolateyPackage @packageArgs
