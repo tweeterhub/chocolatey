01a8ccce3bf6ea78d0cac1439b7531d8c1dcb98a
Sebastien Boulianne <sebastien.boulianne@cpu.ca>
2019-10-16 12:24:24 -0400

    .

---
+++ packages/tinypdf/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'TinyPDF'
+$Version = "3.00.3200"
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'MSI'
+  url64bit      = "http://www.tinypdf.com/InstallTinyPDF.exe"
+  softwareName  = "TinyPDF*"
+  checksum64    = '261068B945D44CBE029D2553CACAF4665BC30EF31B321D82A25CD87A71E18F7C'
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+  }
+
+Install-ChocolateyPackage @packageArgs
