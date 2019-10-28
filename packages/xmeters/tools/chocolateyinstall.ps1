596aed221065a9ad29ba5ef4bfc5de8824655aaf
Sebastien Boulianne <boubou@upsidehosting.com>
2019-10-28 10:55:44 -0400

    .

--- packages/xmeters/tools/chocolateyinstall.ps1
+++ packages/xmeters/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   checksum = '7E88AC356DBCF8B33A17D52AB0178EA516471E7BC4D7D1C9814104FD74D584B3'
   checksumType  = 'sha256'
   silentArgs    = "/S"
-  validExitCodes= @(0)
+  validExitCodes= @(0, 1116, 1641, 3010)
   
 }
 
