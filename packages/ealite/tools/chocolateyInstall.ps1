e01c116c3ea2c55d796cdf58175cd38a0c9a220e
Sebastien Boulianne <boubou@upsidehosting.com>
2020-04-01 00:42:19 -0400

    .

--- packages/ealite/tools/chocolateyInstall.ps1
+++ packages/ealite/tools/chocolateyInstall.ps1
@@ -1,20 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
-
-$packageName= '{{PackageName}}'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = '{{DownloadUrl}}'
-
+$url        = 'https://www.sparxsystems.com/bin/easetup.msi'
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
+  packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
   url           = $url
-
   softwareName  = 'Enterprise Architect*'
-
-  checksum      = '{{Checksum}}'
+  checksum      = '72B9DBD2A47BD3EC5EDAB40A1F302A0EE10A4333534EA3C05E136D1BF9DB86B5'
   checksumType  = 'sha256'
-  
+  silentArgs    = "/quiet"
+  validExitCodes= @(0)
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
