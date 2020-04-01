9cd6b10990acd054c09f8fdf4bcc1fc8aee7569d
Sebastien Boulianne <boubou@upsidehosting.com>
2020-04-01 16:01:57 -0400

    .

--- packages/ealite/tools/chocolateyInstall.ps1
+++ packages/ealite/tools/chocolateyInstall.ps1
@@ -7,7 +7,6 @@ $packageArgs = @{
   softwareName  = 'Enterprise Architect*'
   checksum      = '72B9DBD2A47BD3EC5EDAB40A1F302A0EE10A4333534EA3C05E136D1BF9DB86B5'
   checksumType  = 'sha256'
-  silentArgs    = "/quiet"
   validExitCodes= @(0)
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
