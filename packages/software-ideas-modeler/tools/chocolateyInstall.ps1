052fa0ed2433ae736297123c490af013c8c0a429
Sebastien Boulianne <boubou@upsidehosting.com>
2020-04-01 19:48:33 -0400

    .

--- packages/software-ideas-modeler/tools/chocolateyInstall.ps1
+++ packages/software-ideas-modeler/tools/chocolateyInstall.ps1
@@ -13,8 +13,8 @@ $packageArgs = @{
   checksumType  = 'sha256'
   checksum64    = '54EDEDE275E559E7BD897579216AC39D33253B4DF832D422A36C4AC3A601E740'
   checksumType64= 'sha256'
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  validExitCodes= @(0)
   }
 
 Install-ChocolateyPackage @packageArgs
