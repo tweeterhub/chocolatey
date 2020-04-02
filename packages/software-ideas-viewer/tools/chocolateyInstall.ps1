e3d01a59db4f8605bfe743ffc906d3d5d2c99d4e
Sebastien Boulianne <boubou@upsidehosting.com>
2020-04-01 21:37:05 -0400

    .

--- packages/software-ideas-viewer/tools/chocolateyInstall.ps1
+++ packages/software-ideas-viewer/tools/chocolateyInstall.ps1
@@ -1,23 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
-
-$packageName= '{{PackageName}}'
-$url        = '{{DownloadUrl}}'
-
+$packageName= 'Software Ideas Viewer'
+$Version = "12.47"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
-
-  softwareName  = 'Software Ideas Viewer*'
-
-  checksum      = '{{Checksum}}'
+  url           = "https://www.softwareideas.net/Download/File/2037/SoftwareIdeasViewerSetup32-12-47.exe"
+  softwareName  = "Software Ideas Viewer*"
+  checksum      = '3CC621BEA105756B3296F2A64320AE9D03BA54D61C7C6117B09A947819EA5194'
   checksumType  = 'sha256'
-
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   validExitCodes= @(0)
-}
+  }
 
 Install-ChocolateyPackage @packageArgs
