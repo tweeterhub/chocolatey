5520e4e5953254b6a5b5fdc26ecd7836053d490e
Sebastien Boulianne <boubou@upsidehosting.com>
2020-04-18 22:53:15 -0400

    .

--- packages/bria-solo-free/tools/chocolateyInstall.ps1
+++ packages/bria-solo-free/tools/chocolateyInstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   softwareName  = "Bria*"
   checksum      = '9F1B917150FAF49AA9866002542CEF15141A2DBFCB09FB4C7D03BFC926CEF757'
   checksumType  = 'sha256'
-  silentArgs    = "--Silent --AcceptEULA=YES"
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART"
   validExitCodes= @(0)
   }
 
