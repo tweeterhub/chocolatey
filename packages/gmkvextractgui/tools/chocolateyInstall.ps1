e04b640f68377028daf4fefcf5ee2e20efc22d53
boubou <boubou@upsidehosting.com>
2020-06-04 10:47:29 -0400

    .

--- packages/gmkvextractgui/tools/chocolateyInstall.ps1
+++ packages/gmkvextractgui/tools/chocolateyInstall.ps1
@@ -1,9 +1,14 @@
-﻿$packageName    = '{{PackageName}}'
-$url            = '{{DownloadUrl}}'
-$checksum       = '{{Checksum}}'
-$checksumType   = 'sha256'
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'registryfilevalidator'
+$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+    PackageName    = $packageName
+    Url64bit       = 'https://www.videohelp.com/download/gMKVExtractGUI.v2.5.2.7z'
+    Checksum64     = '81031962B341AD9FB0E46E9AE8E5B63CEA339DCD1DE5388739DF0AD7BD1A744F'
+    ChecksumType64 = 'sha256'
+    UnzipLocation  = $toolsDir
+}
 
-
-Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$toolsDir" -ChecksumType "$checksumType" -Checksum "$checksum"
+# Download and unzip into a temp folder
+Install-ChocolateyZipPackage @packageArgs
