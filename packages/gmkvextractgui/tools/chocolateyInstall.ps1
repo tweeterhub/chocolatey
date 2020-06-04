e8305a826b9360092ef03e1d5420824b354d1c49
boubou <boubou@upsidehosting.com>
2020-06-04 10:33:09 -0400

    .

---
+++ packages/gmkvextractgui/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName    = '{{PackageName}}'
+$url            = '{{DownloadUrl}}'
+$checksum       = '{{Checksum}}'
+$checksumType   = 'sha256'
+
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$toolsDir" -ChecksumType "$checksumType" -Checksum "$checksum"
