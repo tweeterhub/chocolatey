2adbac935c8fbb0a79b606ed0071ce77e6a989e7
boubou <boubou@upsidehosting.com>
2020-07-31 20:21:17 -0400

    .

--- packages/wakemeonlan/tools/chocolateyInstall.ps1
+++ packages/wakemeonlan/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
-$packageName= 'Software Ideas Viewer'
-$Version = "12.47"
+$packageName= 'WakeMeOnLAN'
+$Version = "1.86"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $packageName
-  fileType      = 'EXE'
-  url           = "https://www.softwareideas.net/Download/File/2037/SoftwareIdeasViewerSetup32-12-47.exe"
-  softwareName  = "Software Ideas Viewer*"
-  checksum      = '3CC621BEA105756B3296F2A64320AE9D03BA54D61C7C6117B09A947819EA5194'
+  fileType      = 'ZIP'
+  url           = "https://www.nirsoft.net/utils/wakemeonlan-x64.zip"
+  softwareName  = "WakeMeOnLAN*"
+  checksum      = '1C8FA56C055E2F570CED5B7B2E2EAAD3E46ECBA03EED61A200D09F21ADB35926'
   checksumType  = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   validExitCodes= @(0)
