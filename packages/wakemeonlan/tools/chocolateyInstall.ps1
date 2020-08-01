889bddf800eaeddc91fe26548ddb0c29e1ca5799
boubou <boubou@upsidehosting.com>
2020-07-31 20:42:34 -0400

    .

--- packages/wakemeonlan/tools/chocolateyInstall.ps1
+++ packages/wakemeonlan/tools/chocolateyInstall.ps1
@@ -4,14 +4,12 @@ $Version = "1.86"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'ZIP'
-  url           = "https://www.nirsoft.net/utils/wakemeonlan-x64.zip"
-  softwareName  = "WakeMeOnLAN*"
-  checksum      = '1C8FA56C055E2F570CED5B7B2E2EAAD3E46ECBA03EED61A200D09F21ADB35926'
-  checksumType  = 'sha256'
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  validExitCodes= @(0)
-  }
+    PackageName    = $packageName
+    Url64          = 'https://www.nirsoft.net/utils/wakemeonlan-x64.zip'
+    Checksum64     = '1C8FA56C055E2F570CED5B7B2E2EAAD3E46ECBA03EED61A200D09F21ADB35926'
+    ChecksumType64 = 'sha256'
+    UnzipLocation  = $toolsDir
+}
 
-Install-ChocolateyPackage @packageArgs
+# Download and unzip into a temp folder
+Install-ChocolateyZipPackage @packageArgs
