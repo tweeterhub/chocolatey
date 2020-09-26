d7ab9bb2c92aee18b1ca423e0672ec48305e9937
boubou <boubou@upsidehosting.com>
2020-09-26 14:08:48 -0400

    .

--- packages/profwiz/tools/chocolateyInstall.ps1
+++ packages/profwiz/tools/chocolateyInstall.ps1
@@ -1,17 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
-$packageName= 'Wayk Now'
-$Version = "2019.2.1.0"
+$packageName= 'Profwiz'
+$Version = "3.20"
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'MSI'
-  url           = "https://cdn.devolutions.net/download/Wayk/2019.2.1.0/WaykNow-x86-2019.2.1.0.msi#_ga=2.10116147.725310804.1573225798-1075188698.1557089024"
-  url64bit      = "https://cdn.devolutions.net/download/Wayk/2019.2.1.0/WaykNow-x64-2019.2.1.0.msi#_ga=2.10116147.725310804.1573225798-1075188698.1557089024"
-  softwareName  = "Wayk Now*"
-  checksum      = '7B63C6240E0731313E2C3931EF14E475BFCD28F243A349ABFDD78BEA3731C867'
+  url           = "https://www.forensit.com/Downloads/Profwiz.msi"
+  softwareName  = "Profwiz*"
+  checksum      = 'CCAF23FE56152F75866B1F01B3B9D0DD7D83FB05342BE8D6B9CFE999ADDDBE99'
   checksumType  = 'sha256'
-  checksum64    = '2FA20998218FFA397A4705AA70B121EAB89A7DBEF804D79FC7FAE2F1C7AE3CD4'
-  checksumType64= 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
   }
