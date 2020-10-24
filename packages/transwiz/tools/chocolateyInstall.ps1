b85476ad7b97a9018fe03260022b0102757126de
boubou <boubou@upsidehosting.com>
2020-10-23 23:53:58 -0400

    .

---
+++ packages/transwiz/tools/chocolateyInstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.forensit.com/Downloads/Transwiz.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'Transwiz*'
+  checksum      = 'E9AE0FB056EF6E18AB257F1AAC795F08E5900BFBA817ED1EADDB712B09879EE0'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
