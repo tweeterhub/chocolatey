cdd1c1c5245e0f26da9593cd4c0283b14a8bb571
boubou <boubou@upsidehosting.com>
2020-10-25 17:40:41 -0400

    .

---
+++ packages/defprof/tools/chocolateyInstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.forensit.com/Downloads/Support/DefProf.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'Defprof*'
+  checksum      = '83E440B791AE26F70B70EA58D9A28DF4D5B77734F616A86DC77E50F455A03CAF'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
