b6768cdff66cacef03298d92d72d093e1420bf2b
boubou <boubou@upsidehosting.com>
2020-10-25 20:14:32 -0400

    .

--- packages/sonicwall.global.vpn-client/tools/chocolateyInstall.ps1
+++ packages/sonicwall.global.vpn-client/tools/chocolateyInstall.ps1
@@ -13,7 +13,7 @@ $packageArgs = @{
   checksumType  = 'sha256'
   checksum64    = '9C2398A05EAB9C3F97AFA68E2D9F2AF8DD5312A770822AD735BA72F1FDDDDF81'
   checksumType64= 'sha256'
-  silentArgs   = '/S'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0)
   }
 
