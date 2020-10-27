bb7a59a2a73551f51314b0e94a9c97ac09bf5924
boubou <boubou@upsidehosting.com>
2020-10-27 19:38:43 -0400

    .

---
+++ packages/jabra-direct/tools/chocolateyinstall.ps1
@@ -0,0 +1,32 @@
+﻿# jabra-direct install
+
+$ErrorActionPreference = 'Stop';
+
+$toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$PackageParameters   = Get-PackageParameters
+$urlPackage = 'https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.exe'
+$checksumPackage = '4C192DC513A609242541446AF32B4E3B'
+$checksumTypePackage = 'SHA512'
+
+Import-Module -Name "$($toolsDir)\helpers.ps1"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'EXE'
+    url            = $urlPackage
+    checksum       = $checksumPackage
+    checksumType   = $checksumTypePackage
+	silentArgs     = '/install /quiet /norestart'
+    validExitCodes = @(0, 1000, 1101)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+if ($PackageParameters.CleanStartmenu) {
+	Remove-FileItem `
+		-Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Jabra\"
+    Install-ChocolateyShortcut `
+        -ShortcutFilePath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Jabra Direct.lnk" `
+        -TargetPath "C:\Program Files (x86)\Jabra\Direct\JabraDirect.exe" `
+        -WorkDirectory "C:\Program Files (x86)\Jabra\Direct\"
+}	
