789d100adcdd77d714c5cf21cbcc317f12090155
boubou <boubou@upsidehosting.com>
2020-10-27 20:57:53 -0400

    .

---
+++ packages/kingston-ssd-manager/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url                    = 'https://media.kingston.com/support/downloads/KSM_setup_1.1.2.6.exe'
+  checksum               = '21110878E93FD7A15EFD3667103BDED7E67EB87AC83DBEB55464CCDC5C0442F1'
+  checksumType           = 'sha256'
+}
+Install-ChocolateyPackage @packageArgs
---
+++ packages/kingston-ssd-manager/update.ps1
@@ -0,0 +1,24 @@
+
+import-module au
+import-module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+	$URL32 = 'https://media.kingston.com/support/downloads/KSM-setup.exe'
+    Get-ChocolateyWebFile 'kingston-ssd-manager' $URL32 -FileFullPath "$env:TMP\KSM-setup.exe"
+	
+	@{
+        URL32   = $URL32
+        Version = (Get-Item "$env:TMP\KSM-setup.exe").VersionInfo.ProductVersion.trim()
+    }
+}
+
+update
