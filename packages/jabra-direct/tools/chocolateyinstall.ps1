353a26295a93c7ba385371e122a9bd4c0d0da1c0
boubou <boubou@upsidehosting.com>
2020-10-30 10:48:51 -0400

    .

--- packages/jabra-direct/tools/chocolateyinstall.ps1
+++ packages/jabra-direct/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $toolsDir            = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)
 $PackageParameters   = Get-PackageParameters
 $urlPackage = 'https://jabraxpressonlineprdstor.blob.core.windows.net/jdo/JabraDirectSetup.exe'
 $checksumPackage = 'FA4C50DCC86BA853FEB08C5887427F75BAFC456EC2B96280D00260AAD0CDE6A1'
-$checksumTypePackage = 'SHA512'
+$checksumTypePackage = 'sha256'
 
 Import-Module -Name "$($toolsDir)\helpers.ps1"
 
