648d5ba0e3760ef7bedf85bb18a8f8b3a1e271e6
boubou <boubou@upsidehosting.com>
2020-11-14 22:32:35 -0500

    .

---
+++ packages/qfinderpro/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'qsync*'
+  fileType       = 'EXE'
+  url            = 'https://download.qnap.com/Storage/Utility/QNAPQsyncClientWindows-4.5.1.1026.exe'  
+  checksum       = '0569bd5049656c2d3a640f80491a2932c926b038f316ce209213b2e4809ba47c'
+  checksumType   = 'sha256'
+  silentArgs     = "/S"
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
