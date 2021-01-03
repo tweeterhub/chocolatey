81bc8e596cba6365b80b3fdf4defb3a51c1076c1
boubou <boubou@upsidehosting.com>
2021-01-03 15:52:20 -0500

    Aruba4.0.3.0.200911

--- packages/arubaviaclient/4.0.3.0.2009111/tools/chocolateyInstall.ps1
+++ packages/arubaviaclient/4.0.3.0.2009111/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 $packageName= 'Aruba Networks Virtual Intranet Access Client'
-$Version = "4.0.3.0.2009111"
+$Version = "3.4.2.0.1912113"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'MSI'
-  url64bit      = "http://h30326.www3.hpe.com/hpn/Aruba-VIA-4.0.3.0.2009111-64.msi?merchantId=ASP_DROPBOX"
+  url64bit      = "http://h30326.www3.hpe.com/hpn/Aruba-VIA-4.0.0.0.2004212-64.msi?merchantId=ASP_DROPBOX"
   softwareName  = "Aruba Networks Virtual Intranet Access Client*"
-  checksum64    = '58B0B13054D9A35B3FC242B09396A35FC12C659AD38222E19E90F5A636946DC6'
+  checksum64    = 'F3E3402C886A4EABF5270DA62BD4F457FA68224EE0C40FBB3232F72B75414608'
   checksumType64= 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
