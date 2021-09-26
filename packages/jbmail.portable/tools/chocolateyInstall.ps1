8b120e1828b2dc1f19ab6bba13a896c1a3cc3a3a
boubou <boubouqc@bell.net>
2021-09-25 23:54:33 -0400

    .

---
+++ packages/jbmail.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'Software Ideas Modeler.portable'
+$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    PackageName    = $packageName
+    Url64          = 'http://www.pc-tools.net/files/win32/freeware/jbmailplus33.zip'
+    Checksum64     = 'A65EA41DB746FF7868BF6486C21473A79A20A375653311ADFEC54FC014D89E58'
+    ChecksumType64 = 'sha256'
+    UnzipLocation  = $toolsDir
+}
+
+# Download and unzip into a temp folder
+Install-ChocolateyZipPackage @packageArgs
