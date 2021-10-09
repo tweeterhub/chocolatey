254feba62257c5fc0545a263113afd8b6a6326ef
boubou <boubouqc@bell.net>
2021-10-09 00:16:23 -0400

    Windows 11 Installation Assistant fix

--- packages/windows-11-installation-assistant/tools/chocolateyinstall.ps1
+++ packages/windows-11-installation-assistant/tools/chocolateyinstall.ps1
@@ -1,14 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
-$packageName= 'Windows 11 Installation Assistant'
-$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.microsoft.com/download/3/a/9/3a9e2fe1-96e7-4514-8744-f3a9731f91c7/Windows11InstallationAssistant.exe'
 
 $packageArgs = @{
-    PackageName    = $packageName
-    Url64          = 'https://download.microsoft.com/download/3/a/9/3a9e2fe1-96e7-4514-8744-f3a9731f91c7/Windows11InstallationAssistant.exe'
-    Checksum64     = '9D04E6C0EEF448763AD50BF094C8076F4CE6673A83232FB200355F20F77F021A'
-    ChecksumType64 = 'sha256'
-    UnzipLocation  = $toolsDir
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP'
+  url           = $url
+  softwareName  = 'Windows 11*'
+  checksum      = '9D04E6C0EEF448763AD50BF094C8076F4CE6673A83232FB200355F20F77F021A'
+  checksumType  = 'sha256'
 }
 
-# Download and unzip into a temp folder
 Install-ChocolateyZipPackage @packageArgs
