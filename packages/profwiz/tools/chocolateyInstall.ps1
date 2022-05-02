b8e0aa98cd5d5954d78a470f287d80e16403f363
BouBou <boubouqc@bell.net>
2022-05-02 13:01:36 -0400

    3.24

--- packages/profwiz/tools/chocolateyInstall.ps1
+++ packages/profwiz/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
-$packageName= 'Profwiz'
-$Version = "3.24"
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.forensit.com/Downloads/Profwiz.msi'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'MSI'
-  url           = "https://www.forensit.com/Downloads/Profwiz.msi"
-  softwareName  = "Profwiz*"
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'Profwiz*'
   checksum      = 'BC2BCF84E3342F85A6E56FA29558525469BF615B1DDF3D0367D7DC6B7160BD0A'
   checksumType  = 'sha256'
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
-  }
 
-Install-ChocolateyPackage @packageArgs
+}
+
+Install-ChocolateyZipPackage @packageArgs
