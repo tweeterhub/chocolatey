52525000ac686d8749856aba868f587bd6dfada6
BouBou <boubouqc@bell.net>
2022-05-03 20:52:11 -0400

    3.24

--- packages/profwiz/tools/chocolateyInstall.ps1
+++ packages/profwiz/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'Profwiz*'
-  checksum      = 'BC2BCF84E3342F85A6E56FA29558525469BF615B1DDF3D0367D7DC6B7160BD0A'
+  checksum      = '3F49A09D6B1CBBA079D928E5BC7D2F716F57AC150540E0CF37E4B4517310B988'
   checksumType  = 'sha256'
 
 }
