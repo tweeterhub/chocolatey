e050bad98be37dbdde862ea622281b32fba8b81e
tweeterhub <97837604+tweeterhub@users.noreply.github.com>
2022-05-25 10:29:19 +0200

    Update chocolateyinstall.ps1
    Update to version 2.5.4594.1

--- packages/advancedipscanner/tools/chocolateyinstall.ps1
+++ packages/advancedipscanner/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-﻿$packageArgs = @{
+$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'exe'
   SilentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  url                    = 'https://download.advanced-ip-scanner.com/download/files/Advanced_IP_Scanner_2.5.3850.exe'
-  checksum               = '87BFB05057F215659CC801750118900145F8A22FA93AC4C6E1BFD81AA98B0A55'
+  url                    = 'https://download.advanced-ip-scanner.com/download/files/Advanced_IP_Scanner_2.5.4594.1.exe'
+  checksum               = '26d5748ffe6bd95e3fee6ce184d388a1a681006dc23a0f08d53c083c593c193b'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
